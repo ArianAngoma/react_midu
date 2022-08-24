d88764e96def6d15367eb6366279d07bd173e5fb
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-08-24 08:26:49 -0500

    Object in state

--- giffy/src/components/ListOfGifs.js
+++ giffy/src/components/ListOfGifs.js
@@ -4,27 +4,31 @@ import {getGifts} from "../services/getGifs";
 import {Gif} from "./Gif";
 
 export const ListOfGifs = ({params}) => {
-    const [loading, setLoading] = useState(false);
 
     const {keyword} = params
 
-    const [gifs, setGifs] = useState([]);
+    const [gifs, setGifs] = useState({
+        loading: false,
+        results: []
+    });
 
     useEffect(() => {
         console.log('useEffect');
-        setLoading(true);
+
+        setGifs((prevState) => ({...prevState, loading: true}));
+
         getGifts({keyword}).then(gifs => {
             setGifs(gifs)
-            setLoading(false);
+            setGifs((prevState) => ({...prevState, loading: false, results: gifs}));
         });
     }, [keyword]);
 
-    if (loading) return <p>Loading...</p>;
+    if (gifs.loading) return <p>Loading...</p>;
 
     return (
         <>
             {
-                gifs.map(({id, title, url}) => <Gif
+                gifs.results.map(({id, title, url}) => <Gif
                     key={id}
                     id={id}
                     title={title}
