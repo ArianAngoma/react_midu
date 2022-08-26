7e06093608bb1a7417ab7d38cdca7535ee1a70d7
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-08-26 13:53:18 -0500

    Custom Hook - useGlobalGifs

--- giffy/src/pages/Detail/index.js
+++ giffy/src/pages/Detail/index.js
@@ -1,9 +1,9 @@
-import {useContext} from "react";
-import {GifsContext} from "../../context/GifsContext";
+
 import {Gif} from "../../components/Gif/Gif";
+import {useGlobalGifs} from "../../hooks/useGlobalGifs";
 
 export const Detail = ({params}) => {
-    const {gifs} = useContext(GifsContext)
+    const gifs = useGlobalGifs()
 
     const gif = gifs.find(gif => gif.id === params.id);
 
