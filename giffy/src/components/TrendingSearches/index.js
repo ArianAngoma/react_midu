17c4486bf49e54f1e4df7bbdf4613a81f5a6ba44
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-08-29 14:00:20 -0500

    Custom Hook - useNearScreen

--- giffy/src/components/TrendingSearches/index.js
+++ giffy/src/components/TrendingSearches/index.js
@@ -1,6 +1,7 @@
-import {useEffect, useRef, useState} from "react";
+import {useEffect, useState} from "react";
 import {getTrendingTermsService} from "services/getTrendingTermsService";
 import {Category} from "../Category";
+import {useNearScreen} from "hooks/useNearScreen";
 
 const TrendingSearches = () => {
     const [trends, setTrends] = useState([]);
@@ -17,40 +18,6 @@ const TrendingSearches = () => {
     )
 }
 
-const useNearScreen = ({distance =  '100px'} = {}) => {
-    const [isNearScreen, setShow] = useState(false);
-    const fromRef = useRef()
-
-    useEffect(() => {
-        let observer
-
-        const onChange = (entries, observer) => {
-            const el = entries[0];
-            console.log(el.isIntersecting)
-            if (el.isIntersecting) {
-                setShow(true)
-                observer.disconnect();
-            }
-        }
-
-        Promise.resolve(
-            typeof IntersectionObserver !== 'undefined'
-                ? IntersectionObserver
-                : import('intersection-observer')
-        ).then(() => {
-            observer = new IntersectionObserver(onChange, {
-                rootMargin: distance
-            })
-
-            observer.observe(fromRef.current);
-        })
-
-
-        return () => observer && observer.disconnect();
-    })
-
-    return {isNearScreen, fromRef}
-}
 
 export const LazyTrending = () => {
     const {isNearScreen, fromRef} = useNearScreen({distance: '200px'})
