c4121d534a6da0317ab7bd7fbb525dd58a1d5bae
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-09-04 18:34:57 -0500

    Styles Login

---
+++ src/components/Login/index.js
@@ -0,0 +1,78 @@
+import {useEffect, useState} from "react";
+import {useLocation} from "wouter";
+
+import './styles.css';
+
+import useUser from "hooks/useUser";
+
+export default function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [, navigate] = useLocation();
+
+    const {login, isLogged, isLoginLoading, hasLoginError} = useUser()
+
+    useEffect(() => {
+        if (isLogged) navigate('/')
+    }, [isLogged, navigate])
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        login({username, password})
+    }
+
+    if (isLoginLoading) return <p>Loading...</p>
+
+    return (
+        <>
+
+            {
+                isLoginLoading && <strong>Checking credentials...</strong>
+            }
+
+            {
+                !isLoginLoading && (
+                    <form
+                        onSubmit={handleSubmit}
+                        className="form"
+                    >
+
+                        <label>
+                            username
+
+                            <input
+                                placeholder="username"
+                                type="text"
+                                value={username}
+                                onChange={(event) => setUsername(event.target.value)}
+                            />
+                        </label>
+
+                        <label>
+                            password
+
+                            <input
+                                placeholder="password"
+                                type="password"
+                                value={password}
+                                onChange={(event) => setPassword(event.target.value)}
+                            />
+                        </label>
+
+
+
+                        <button className="btn">Login</button>
+
+                    </form>
+                )
+            }
+
+            {
+                hasLoginError && <strong>Invalid credentials</strong>
+            }
+
+        </>
+    )
+}
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,63 +1,10 @@
-import {useEffect, useState} from "react";
-import {useLocation} from "wouter";
-import useUser from "../../hooks/useUser";
+import Login from "components/Login";
 
-export default function Login() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-
-    const [, navigate] = useLocation();
-
-    const {login, isLogged, isLoginLoading, hasLoginError} = useUser()
-
-    useEffect(() => {
-        if (isLogged) navigate('/')
-    }, [isLogged, navigate])
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-
-        login({username, password})
-    }
-
-    if (isLoginLoading) return <p>Loading...</p>
-
+export default function LoginPage() {
     return (
         <>
             <h2>Login</h2>
-
-            {
-                isLoginLoading && <strong>Checking credentials...</strong>
-            }
-
-            {
-                !isLoginLoading && (
-                    <form onSubmit={handleSubmit}>
-
-                        <input
-                            placeholder="username"
-                            type="text"
-                            value={username}
-                            onChange={(event) => setUsername(event.target.value)}
-                        />
-
-                        <input
-                            placeholder="password"
-                            type="password"
-                            value={password}
-                            onChange={(event) => setPassword(event.target.value)}
-                        />
-
-                        <button>Login</button>
-
-                    </form>
-                )
-            }
-
-            {
-                hasLoginError && <strong>Invalid credentials</strong>
-            }
-
+            <Login/>
         </>
     )
 }
